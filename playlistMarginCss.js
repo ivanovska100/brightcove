51b7013098228717dae580681053542d26eaf321
iivanovska <ivana.ivanovska@endava.com>
2020-05-06 20:03:03 +0200

    play-icon-js

--- playlistMarginCss.js
+++ playlistMarginCss.js
@@ -14,6 +14,19 @@ videojs.registerPlugin('playlistItemMargin', function(options) {
                                     playlistItems[i].style.marginBottom = titleHeight + 'px';
                                     i++;
                                 }
+	var videoThumbnails = document.getElementsByClassName('vjs-playlist-thumbnail');
+							   var numberOfvideoThumbnails = playlistItems.length;
+                               var j = 0;
+                               while (j<numberOfvideoThumbnails)
+                               {                                 
+                                    videoThumbnails[j].addEventListener("mouseover", function(e) {
+									videoThumbnails[j].classList.add("vjs-icon-play");
+									});
+									videoThumbnails[j].addEventListener("mouseout", function(e) {
+									videoThumbnails[j].classList.remove("vjs-icon-play");
+									});
+                                    j++;
+                               }
 });
 
 });
