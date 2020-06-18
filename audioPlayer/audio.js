6497086bb69a061427fae24786db6d99c0b6ec59
Ivana Ivanovska <ivana.ivanovska@endava.com>
2020-06-18 16:29:42 +0200

    audioPlugin

---
+++ audioPlayer/audio.js
@@ -0,0 +1,20 @@
+videojs.registerPlugin('audio', function(options) {
+  
+	var myPlayer = this;
+
+	myPlayer.on("loadstart", function() {
+	var playlistItems = document.getElementsByClassName('vjs-playlist-item');
+      
+	myPlayer.getChild('bigPlayButton').controlText('Play Audio');
+	
+	});
+	
+    myPlayer.textTrackSettings.setValues({
+      //backgroundColor: "#FF0",
+      //color: "#00F",
+      //fontFamily: "monospaceSansSerif",
+      fontPercent: 4.0,
+      windowColor: "#0FF",
+      windowOpacity: "0.5"
+    }) ;
+});
